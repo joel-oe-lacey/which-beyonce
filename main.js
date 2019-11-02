669ebc363ac111a693b8f96f0aad574809135087
Joel Lacey <joellaceybusiness@gmail.com>
2019-11-01 19:38:10 -0600

    Display game after instructions proceed

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ var playerSubmit = document.querySelector('.intro__button--play');
 var nameIntro = document.querySelector('.intro');
 var instructions = document.querySelector('.instructions');
 var instructionHeader = document.querySelector('.instructions__header');
+var instructionPlay = document.querySelector('.instructions__button--play');
+var centerDiv = document.querySelector('.center');
+var game = document.querySelector('.game');
+var gamePlayer1Header = document.querySelector('.player1__h2');
 
-//shows up while typing until next input, fix that my moving to the input itself?
 playerSubmit.addEventListener("click", checkNameInput);
+instructionPlay.addEventListener("click", showGame);
 
 function checkNameInput() {
   if (player1Input.validity.valueMissing) {
+    //shows up while typing until next input, fix that my moving to the input itself?
     player1Input.setCustomValidity("Please enter a name");
     player1Input.reportValidity();
   } else {
@@ -22,8 +27,15 @@ function checkNameInput() {
 };
 
 function showInstructions() {
-  var player1Name = player1Input.value;
+  player1Name = player1Input.value;
   instructionHeader.innerText = `Welcome ${player1Name} and Player 2 Name!`;
   nameIntro.classList.add('hidden');
   instructions.classList.remove('hidden');
-}
+};
+
+function showGame() {
+  instructions.classList.add('hidden');
+  centerDiv.classList.add('hidden');
+  gamePlayer1Header.innerText = `${player1Name}`;
+  game.classList.remove('hidden');
+};
